3b9bb087067b977be68416f935934ffbbe37e409
sb <stefan.busch@sb-systec.de>
2026-01-12 11:28:24 +0100

    Disable parallelization for specific xUnit tests by adding `NonParallelTestCollection`.

---
+++ r47.Primitives.EnumT.Test/NonParallelTestCollection.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace r47.Primitives.EnumT.Test
+{
+    // Define a collection for tests that need to run sequentially
+    [CollectionDefinition("parallel-execution", DisableParallelization = true)]
+    public class NonParallelTestCollection
+    {
+        // This class remains empty.
+        // It serves solely as a marker for xUnit Test Collection.        
+    }
+}
